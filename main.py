0884d91d43e81f6cd67a45b9f141704caceec420
saklaniaman <saklaniaman2001@gmail.com>
2024-11-11 15:45:00 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_dataIngestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_dataValidation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_dataTransformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_modelTrainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_modelEvaluation import ModelEvaluationTrainingPipeline
 
 
 
@@ -34,6 +36,28 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
